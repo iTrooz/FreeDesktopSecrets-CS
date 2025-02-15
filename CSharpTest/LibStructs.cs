d74704e0c99d7b61e956449ce66341b377437252
iTrooz <hey@itrooz.fr>
2025-02-15 18:12:55 +0100

    make it actually work

---
+++ CSharpTest/LibStructs.cs
@@ -0,0 +1,202 @@
+using Tmds.DBus;
+
+namespace CSharpTest;
+
+
+[DBusInterface("org.freedesktop.Secret.Service")]
+public interface IService : IDBusObject
+{
+  Task<(object output, ObjectPath result)> OpenSessionAsync(string algorithm, object input);
+  Task<(ObjectPath[] unlocked, ObjectPath prompt)> UnlockAsync(ObjectPath[] objects);
+  Task<T> GetAsync<T>(string prop);
+}
+
+[DBusInterface("org.freedesktop.Secret.Collection")]
+public interface ICollection : IDBusObject
+{
+  Task<(ObjectPath item, ObjectPath prompt)> CreateItemAsync(IDictionary<string, object> properties, (ObjectPath, byte[], byte[], string) secret, bool replace);
+  Task<CollectionProperties> GetAllAsync();
+  Task<ObjectPath[]> SearchItemsAsync(IDictionary<string, string> Attributes);
+}
+
+[Dictionary]
+public class CollectionProperties
+{
+  private ObjectPath[] _Items = default(ObjectPath[]);
+  public ObjectPath[] Items
+  {
+    get
+    {
+      return _Items;
+    }
+
+    set
+    {
+      _Items = (value);
+    }
+  }
+
+  private string _Label = default(string);
+  public string Label
+  {
+    get
+    {
+      return _Label;
+    }
+
+    set
+    {
+      _Label = (value);
+    }
+  }
+
+  private bool _Locked = default(bool);
+  public bool Locked
+  {
+    get
+    {
+      return _Locked;
+    }
+
+    set
+    {
+      _Locked = (value);
+    }
+  }
+
+  private ulong _Created = default(ulong);
+  public ulong Created
+  {
+    get
+    {
+      return _Created;
+    }
+
+    set
+    {
+      _Created = (value);
+    }
+  }
+
+  private ulong _Modified = default(ulong);
+  public ulong Modified
+  {
+    get
+    {
+      return _Modified;
+    }
+
+    set
+    {
+      _Modified = (value);
+    }
+  }
+}
+
+
+[DBusInterface("org.freedesktop.Secret.Prompt")]
+public interface IPrompt : IDBusObject
+{
+  Task PromptAsync(String windowID);
+  Task<IDisposable> WatchCompletedAsync(Action<(bool dismissed, object result)> handler);
+}
+
+[DBusInterface("org.freedesktop.Secret.Item")]
+public interface IItem : IDBusObject
+{
+  Task<ObjectPath> DeleteAsync();
+  Task<(ObjectPath secret, byte[], byte[], string)> GetSecretAsync(ObjectPath Session);
+  Task SetSecretAsync((ObjectPath, byte[], byte[], string) Secret);
+  Task<T> GetAsync<T>(string prop);
+  Task<ItemProperties> GetAllAsync();
+  Task SetAsync(string prop, object val);
+  Task<IDisposable> WatchPropertiesAsync(Action<PropertyChanges> handler);
+}
+
+[Dictionary]
+public class ItemProperties
+{
+  private bool _Locked = default(bool);
+  public bool Locked
+  {
+    get
+    {
+      return _Locked;
+    }
+
+    set
+    {
+      _Locked = (value);
+    }
+  }
+
+  private IDictionary<string, string> _Attributes = default(IDictionary<string, string>);
+  public IDictionary<string, string> Attributes
+  {
+    get
+    {
+      return _Attributes;
+    }
+
+    set
+    {
+      _Attributes = (value);
+    }
+  }
+
+  private string _Label = default(string);
+  public string Label
+  {
+    get
+    {
+      return _Label;
+    }
+
+    set
+    {
+      _Label = (value);
+    }
+  }
+
+  private string _Type = default(string);
+  public string Type
+  {
+    get
+    {
+      return _Type;
+    }
+
+    set
+    {
+      _Type = (value);
+    }
+  }
+
+  private ulong _Created = default(ulong);
+  public ulong Created
+  {
+    get
+    {
+      return _Created;
+    }
+
+    set
+    {
+      _Created = (value);
+    }
+  }
+
+  private ulong _Modified = default(ulong);
+  public ulong Modified
+  {
+    get
+    {
+      return _Modified;
+    }
+
+    set
+    {
+      _Modified = (value);
+    }
+  }
+}
--- CSharpTest/Program.cs
+++ CSharpTest/Program.cs
@@ -1,7 +1,128 @@
-﻿class Program {
+﻿using CSharpTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tmds.DBus;
 
-  public static void Main(String[] args)
+public struct SecretStruct
+{
+  public ObjectPath ObjectPath { get; set; }
+  public byte[] ByteArray1 { get; set; }
+  public byte[] ByteArray2 { get; set; }
+  public string String { get; set; }
+
+  public static implicit operator (ObjectPath, byte[], byte[], string)(SecretStruct secretStruct)
   {
-    Console.WriteLine("Hey !");
+    return (secretStruct.ObjectPath, secretStruct.ByteArray1, secretStruct.ByteArray2, secretStruct.String);
+  }
+}
+
+public class SecretStorage
+{
+  public const string DEFAULT_COLLECTION = "/org/freedesktop/secrets/aliases/default";
+  public Connection Connection { get; set; }
+  public IService ServiceProxy { get; set; }
+  public ICollection CollectionProxy { get; set; }
+  public ObjectPath Session { get; set; }
+
+  public async Task Init(string collection = DEFAULT_COLLECTION)
+  {
+    Connection = new Connection(Address.Session);
+    await Connection.ConnectAsync();
+    Console.WriteLine("Connected !");
+
+    // Create proxies to call methods
+    ServiceProxy = Connection.CreateProxy<IService>("org.freedesktop.secrets", "/org/freedesktop/secrets");
+    CollectionProxy = Connection.CreateProxy<ICollection>("org.freedesktop.secrets", collection);
+
+    await CreateSession();
+    await UnlockSession();
+  }
+
+  private async Task CreateSession()
+  {
+    Session = (await ServiceProxy.OpenSessionAsync("plain", "my-session")).result;
+    Console.WriteLine($"Created session: {Session}");
+  }
+
+  private async Task UnlockSession()
+  {
+    var (unlocked, unlockPrompt) = await ServiceProxy.UnlockAsync([DEFAULT_COLLECTION]);
+    if (unlockPrompt == "/")
+    {
+      Console.WriteLine("No need to prompt for unlocking");
+    }
+    else
+    {
+      Console.WriteLine("Unlocking failed. Prompting for unlocking");
+      var promptProxy = Connection.CreateProxy<IPrompt>("org.freedesktop.secrets", unlockPrompt);
+      var watch = await promptProxy.WatchCompletedAsync((result) => { Console.WriteLine($"Prompt completed: {result}"); });
+      await promptProxy.PromptAsync("");
+    }
+
+    Console.WriteLine($"Unlocked [{string.Join(", ", unlocked)}] ({unlocked.Length}). Prompt: {unlockPrompt}");
+  }
+
+  public async Task CreateItem()
+  {
+    var secret = new SecretStruct
+    {
+      ObjectPath = this.Session,
+      ByteArray1 = Encoding.UTF8.GetBytes(""),
+      ByteArray2 = Encoding.UTF8.GetBytes("HELLO-THIS-IS-SECRET-2"),
+      String = "text/plain"
+    };
+
+    var (createdItem, prompt) = await this.CollectionProxy.CreateItemAsync(
+      new Dictionary<string, object>
+      {
+        ["application"] = "MyApp/my-app",
+        ["service"] = "MyApp",
+        ["org.freedesktop.Secret.Item.Label"] = "My secret name"
+      },
+      secret, false
+    );
+    Console.WriteLine($"Secret created (createdItem: {createdItem}, prompt: {prompt})");
+  }
+
+  public async Task ListItems()
+  {
+    var props = await this.CollectionProxy.GetAllAsync();
+    foreach (var item in props.Items)
+    {
+      var itemProxy = this.Connection.CreateProxy<IItem>("org.freedesktop.secrets", item);
+      var itemProps = await itemProxy.GetAllAsync();
+      Console.WriteLine($"Item: {item}, Type: {itemProps.Type}, Label: {itemProps.Label}");
+      Console.WriteLine($"Attributes ({itemProps.Attributes.Count}): {string.Join(", ", itemProps.Attributes)}");
+      Console.WriteLine();
+    }
+  }
+  
+  // Doesn't work well, only returns some items
+  public async Task ListItemsLegacy()
+  {
+    var res = await this.CollectionProxy.SearchItemsAsync(new Dictionary<string, string>());
+    Console.WriteLine($"Listing {res.Length} items: {string.Join(", ", res)}");
+    foreach (var item in res)
+    {
+      // Get item
+      var itemProxy = this.Connection.CreateProxy<IItem>("org.freedesktop.secrets", item);
+      var props = await itemProxy.GetAllAsync();
+      Console.WriteLine("----------");
+      Console.WriteLine($"Props: {props.Created}, {props.Modified}, {props.Label}, {props.Type}");
+      Console.WriteLine($"Attributes: {string.Join(", ", props.Attributes)}");
+    }
+  }
+}
+
+class Program
+{
+  static async Task Main()
+  {
+    var secretStorage = new SecretStorage();
+    await secretStorage.Init();
+    await secretStorage.CreateItem();
+    await secretStorage.ListItems();
   }
 }
