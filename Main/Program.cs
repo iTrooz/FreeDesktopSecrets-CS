c974c9d11d8ad53e652396dd6b889e652da27e65
iTrooz <hey@itrooz.fr>
2025-02-16 17:56:25 +0100

    add test Main project

---
+++ Main/Program.cs
@@ -0,0 +1,6 @@
+﻿SecretStorage storage = SecretStorage.FromSession();
+await storage.Connect("TestApplication");
+foreach (var item in await storage.ListItemKeys())
+{
+    Console.WriteLine(item);
+}
