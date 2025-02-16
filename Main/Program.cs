eb55bd709ab5631052b2a308c5972f12dbdbab08
iTrooz <hey@itrooz.fr>
2025-02-17 00:41:55 +0100

    add usage to README

--- Main/Program.cs
+++ Main/Program.cs
@@ -6,12 +6,18 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var storage = SecretStorage.FromSession();
+        var storage = SecretStorage.FromSession(); // use the classic dbus session bus
         storage.Logger = LoggerFactory.Create(builder => builder.AddConsole().SetMinimumLevel(LogLevel.Trace)).CreateLogger("Program");
-        await storage.ConnectAsync("TestApplication");
-        foreach (var item in await storage.ListItemKeysAsync())
-        {
+        await storage.ConnectAsync("TestApplication"); // Use this name for the API "folder" that will hold secrets
+
+        var itemKeys = await storage.ListItemKeysAsync(); // List keys of all secrets stored
+        foreach (var item in itemKeys)
             Console.WriteLine(item);
-        }
+        
+        await storage.CreateItemAsync("TestItem", System.Text.Encoding.UTF8.GetBytes("TestString"), true); // Store a secret
+        var secret = await storage.GetItemAsync("TestItem"); // Retrieve it
+        Console.WriteLine(System.Text.Encoding.UTF8.GetString(secret!)); // "TestString"
+
+        await storage.DeleteItemAsync("TestItem"); // Delete the secret
     }
 }
--- README.md
+++ README.md
@@ -5,3 +5,21 @@ Client library in C# to use the Freedesktop Secret Service API
 
 
 Reference: https://specifications.freedesktop.org/secret-service-spec/latest-single
+
+# Usage
+
+Sample usage of this library:
+```csharp
+var storage = SecretStorage.FromSession(); // use the classic dbus session bus
+await storage.ConnectAsync("TestApplication"); // Use this name for the API "folder" that will hold secrets
+
+var itemKeys = await storage.ListItemKeysAsync(); // List keys of all secrets stored
+foreach (var item in itemKeys)
+    Console.WriteLine(item);
+
+await storage.CreateItemAsync("TestItem", System.Text.Encoding.UTF8.GetBytes("TestString"), true); // Store a secret
+var secret = await storage.GetItemAsync("TestItem"); // Retrieve it
+Console.WriteLine(System.Text.Encoding.UTF8.GetString(secret!)); // "TestString"
+
+await storage.DeleteItemAsync("TestItem"); // Delete the secret
+```
